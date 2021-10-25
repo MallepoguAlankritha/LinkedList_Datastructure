b61560a53568d0fc2088d5b6dcf2e0ec0a9f4018
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 13:55:42 +0530

    Added:Appendind data at end

--- com/bridgelabz/LinkedList/LinkedListMain.java
+++ com/bridgelabz/LinkedList/LinkedListMain.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to linkedList DataStructure Program");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 1 to add the data at the start");
-        switch (scanner.nextInt()) {
-            case 1:
-                Operations.addDataAtStart();
-        }
-    }
-}
+    	 System.out.println("Welcome to linkedList DataStructure Program");
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter 1 to add the data at the start");
+         System.out.println("Enter 2 to add the data at the end");
+         switch (scanner.nextInt()) {
+             case 1:
+                 Operations.addDataAtStart();
+                 break;
+             case 2:
+                 Operations.addDataAtEnd();
+                 break;
+         }
+     }
+ }
