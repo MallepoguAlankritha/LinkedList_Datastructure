5775cea44771f7111a4e52b3d12a30160bb14ae6
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 14:56:52 +0530

    Added:Dequeue element from begining

--- com/bridgelabz/LinkedList/queue/Queue.java
+++ com/bridgelabz/LinkedList/queue/Queue.java
@@ -5,18 +5,32 @@ import com.bridgelabz.LinkedList.LinkedListMain;
 import com.bridgelabz.LinkedList.Node;
 
 public class Queue {
-	private Node top;
-	private LinkedList linkedList;
-   
+	    private Node top;
+	    Node head;
+	    Node tail;
+		private LinkedList linkedList;
+	    
 
-    public Queue() {
-        this.linkedList = new LinkedList();
-    }
+	    public Queue() {
+	        this.linkedList = new LinkedList();
+	    }
 
-    public void enqueue() {
-    	  linkedList.append(56);
-          linkedList.append(30);
-          linkedList.append(70);
-          linkedList.print();
-    }
-}
+	    public void enqueue() {
+	        linkedList.append(56);
+	        linkedList.append(30);
+	        linkedList.append(70);
+	        linkedList.print();
+	    }
+
+	    public void dequeue() {
+	        enqueue();
+	        top = head;
+	        Node temp = top;
+	        while (temp != null) {
+	            linkedList.pop();
+	            temp = temp.next;
+	        }
+	        System.out.println("After");
+	        linkedList.print();
+	    }
+	}
